b6f9df053ebfb1a9114d7107f68149b1c0cada45
camilo-Anthony <camiloanthonyhc@gmail.com>
2026-06-30 20:59:10 -0500

    Cleanup obsolete files and update start/gitignore

--- start.bat
+++ start.bat
@@ -60,7 +60,7 @@ echo [+] Presiona Ctrl+C en la ventana en cualquier momento para apagar a IALena
 echo.
 
 :: Lanzar cliente de voz CLI
-python src\main.py
+python -m src.main
 
 echo.
 echo IALena ha sido apagado.
